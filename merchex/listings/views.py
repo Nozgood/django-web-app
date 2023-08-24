cbd27490d258862e9aee86b811784faa30e036e0
Nowfeel Safi <nowfeel@macbook-air-de-nowfeel.home>
2023-08-24 15:31:38 +0200

    footer + style and contact form POST method

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from listings.models import Band, Listing
+from listings.forms import ContactUsForm
+from django.core.mail import send_mail
 
 def band_list(request):
     bands = Band.objects.all()
@@ -25,7 +27,28 @@ def about_us(request):
     return render(request, 'listings/about.html')
 
 def contact_us(request):
-    return render(request, 'listings/contact.html')
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+
+        if form.is_valid():
+            send_mail(
+                subject=f'Message from {form.cleaned_data["name"] or "anonyme"} via MerchEx Contact Us form',
+                message=form.cleaned_data['message'],
+                from_email=form.cleaned_data['email'],
+                recipient_list=['admin@merchex.xyz'],
+            )
+        return redirect('contact')
+
+    else:
+        form= ContactUsForm()
+
+    return render(
+        request, 
+        'listings/contact.html',
+        {
+            'form': form,
+        }
+    )
 
 def listings(request):
     listings = Listing.objects.all()
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('bands/', views.band_list, name="band_list"),
     path('bands/<int:band_id>/', views.band_detail, name='band_detail'),
     path('about/', views.about_us),
-    path('contact/', views.contact_us),
+    path('contact/', views.contact_us, name="contact"),
     path('listings/', views.listings),
     path('', views.home)
 ]
